d53fa62e1d287c0f84865e6ab17daed2be32fe33
Library User <lib-user@atss-Air.nshealth.ca>
2023-07-21 20:28:27 -0300

    commited the changes for the JS

--- jsBasics/jsBasics.js
+++ jsBasics/jsBasics.js
@@ -1,1 +1,11 @@
+// the line below is used to print strings to the console
 console.log("Hello there!");
+
+let a = true;
+console.log(a);
+
+let firstName = 'Raman';
+console.log(firstName);
+
+firstName = 5;
+console.log(firstName);
