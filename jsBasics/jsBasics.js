96670b9d51090c110d022b6f4ecbc0ca398fa714
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2023-07-25 22:41:03 -0300

    learning more JS

--- jsBasics/jsBasics.js
+++ jsBasics/jsBasics.js
@@ -1,11 +1,37 @@
-// the line below is used to print strings to the console
-console.log("Hello there!");
+// // the line below is used to print strings to the console
+// console.log("Hello there!");
 
-let a = true;
-console.log(a);
+// let a = true;
+// console.log(a);
 
-let firstName = 'Raman';
-console.log(firstName);
+// let firstName = 'Raman';
+// console.log(firstName);
 
-firstName = 5;
-console.log(firstName);
+// firstName = 5;
+// console.log(firstName);
+
+
+// console.log(5<3);
+
+// //Ternary operator
+// let age = 17;
+// let status = (age>18) ? "Can Vote": "Cannot vote";
+// console.log(status);
+
+// If-else-if statements:
+let marks = 98;
+if (marks >= 90) {
+    console.log('A');
+}
+else if (marks >= 80) {
+    console.log('B');
+}
+else if (marks >= 70) {
+    console.log('C');
+}
+else if (marks >= 60) {
+    console.log('D');
+}
+else {
+    console.log('E');
+}
