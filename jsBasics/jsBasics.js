b1a34c9ddd75788a683366d76459325caa7e06d6
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2023-07-26 00:24:08 -0300

    used the loops

--- jsBasics/jsBasics.js
+++ jsBasics/jsBasics.js
@@ -19,19 +19,59 @@
 // console.log(status);
 
 // If-else-if statements:
-let marks = 98;
-if (marks >= 90) {
-    console.log('A');
+// let marks = 98;
+// if (marks >= 90) {
+//     console.log('A');
+// }
+// else if (marks >= 80) {
+//     console.log('B');
+// }
+// else if (marks >= 70) {
+//     console.log('C');
+// }
+// else if (marks >= 60) {
+//     console.log('D');
+// }
+// else {
+//     console.log('E');
+// }
+
+// using switch statements:
+
+let number = 2;
+
+switch (number) {
+    case 1: 
+        console.log('A');
+        break;
+    case 2:
+        console.log('B');
+        break;
+    case 3:
+        console.log('C');
+        break;
+    default:
+        console.log('D');
 }
-else if (marks >= 80) {
-    console.log('B');
+
+// using loops
+// using for loop
+for(let i = 1; i<=5; i++) {
+    console.log('Raman' + i);
 }
-else if (marks >= 70) {
-    console.log('C');
+
+// using while loop
+let j = 0;
+while( j< 10) {
+    console.log(j);
+    j++;
 }
-else if (marks >= 60) {
-    console.log('D');
-}
-else {
-    console.log('E');
-}
+
+// using do while loop
+
+let k = 1;
+do {
+    console.log(k);
+    k++;
+} while(k < 10);
+ 
